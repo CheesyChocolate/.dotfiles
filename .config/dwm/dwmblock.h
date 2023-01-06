5709a8797039b8a0a3e5b5f9401edcae2cbd9643
CheesyChocolate <dev@behnamlal.xyz>
2023-01-06 23:30:28 +0000

    new bluetooth block

--- .config/dwm/dwmblock.h
+++ .config/dwm/dwmblock.h
@@ -20,6 +20,7 @@ static const Block blocks[] = {
 	{"",	"sb-forecast",	18000,	5},
 	{"",	"sb-mailbox",	180,	12},
 	{"",	"sb-nettraf",	1,	16},
+	{"",	"sb-bluetooth",	5,	19},
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	60,	1},
