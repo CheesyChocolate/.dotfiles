9535f21a8b5a7d38cd3dab635a13d97760fd78aa
CheesyChocolate <dev@behnamlal.xyz>
2023-04-13 03:32:36 +0000

    cleaning and rearranging blocks

--- .config/dwm/dwmblock.h
+++ .config/dwm/dwmblock.h
@@ -2,8 +2,8 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"sb-tasks",	10,	26},
-	{"",	"sb-music",	0,	11},
+	{"",	"sb-tasks",		10,	26},
+	{"",	"sb-music",		0,	11},
 	{"",	"sb-pacpackages",	0,	8},
 	{"",	"sb-news",		0,	6},
 	/* {"",	"sb-price lbc \"LBRY Token\" 📚 22",			9000,	22}, */
@@ -13,19 +13,20 @@ static const Block blocks[] = {
 	/* {"",	"sb-price eth Ethereum 🍸 23",				9000,	23}, */
 	/* {"",	"sb-price btc Bitcoin 💰 21",				9000,	21}, */
 	{"",	"sb-torrent",	20,	7},
-	/* {"",	"sb-memory",	10,	14}, */
-	/* {"",	"sb-cpu",		10,	18}, */
-	{"",	"sb-doppler",	0,	13},
-	{"",	"sb-moonphase",	18000,	17},
-	{"",	"sb-forecast",	18000,	5},
+	{"",	"sb-memory",	10,	14},
+	{"",	"sb-cpu",	10,	18},
 	{"",	"sb-mailbox",	180,	12},
 	{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-bluetooth",	5,	19},
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
+	{"",	"sb-doppler",	0,	13},
+	{"",	"sb-moonphase",	18000,	17},
+	{"",	"sb-forecast",	18000,	5},
 	{"",	"sb-clock",	60,	1},
-	{"⌨", "sb-kbselect", 0, 30},
+	{"⌨",	"sb-kbselect",	0,	30},
 	/* {"",	"sb-internet",	5,	4}, */
+	/* {"",	"sb-iplocate", 0,	27}, */
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
@@ -35,4 +36,3 @@ static char *delim = " ";
 // vim with the following line in your vimrc/init.vim:
 
 // autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
-
